554659b58c196c46ee7196aedc6f2a0e74b41fc5
Jon Malmaud <malmaud@gmail.com>
2015-07-04 14:45:37 -0400

    .

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# SQLAlchemy.jl
-Wrapper over Python's SQLAlchemy library
+Wrapper over Python's [SQLAlchemy](http://www.sqlalchemy.org/) library
+
+Basic usage
+============
+
+```julia
+> engine = create_engine("sqlite:///:memory:")
+> metadata = MetaData()
+> users = Table("users", metadata, Column("name", String), Column("age", Real))
+> conn = connect(engine)
+> create_all(metadata, engine)
+> execute(conn, insert(users) |> values(name="Alice", age=27.3))
+> execute(conn, insert(users) |> values(name="Bob", age=45.1))
+> res = execute(conn, select([users]) |> where(users[:age] > 30))
+> fetchall(res)
+1-element Array{Any,1}:
+ PyObject (u'Bob', 45.0)
+```
--- src/SQLAlchemy.jl
+++ src/SQLAlchemy.jl
@@ -1,9 +1,12 @@
 module SQLAlchemy
 
-export Table, Column, Integer, String, MetaData, Engine
-export create_engine, select, text
-export create_all, insert, values, compile, connect, execute, fetchone, fetchall, where, select_from, and_, order_by, alias, join
+using PyCall
+@pyimport sqlalchemy
 
+export Table, Column, Integer, String, MetaData, Engine
+export create_engine, select, text, connect
+export create_all, insert, values, compile, connect, execute, fetchone, fetchall, where, select_from, and_, order_by, alias, join
+export SQLString, SQLInteger, SQLBoolean, SQLDate, SQLDateTime, SQLEnum, SQLFloat, SQLInterval, SQLNumeric, SQLText, SQLTime, SQLUnicode, SQLUnicodeText
 
 abstract Wrapped
 unwrap(x)=x
@@ -14,13 +17,13 @@ function unwrap_kw(x)
     [(v[1], unwrap(v[2])) for v in x]
 end
 
-
 macro wrap_type(typename)
     quote
         type $(esc(typename)) <: Wrapped
             o::PyObject
             function $(esc(typename))(args...; kwargs...)
-                args = map(unwrap, args)
+                args = unwrap(args)
+                kwargs = unwrap_kw(kwargs)
                 new(sqlalchemy.$typename(args...; kwargs...))
             end
             function $(esc(typename))(o::PyObject)
@@ -32,14 +35,40 @@ end
 
 @wrap_type Table
 @wrap_type Column
-@wrap_type Integer
-@wrap_type String
 @wrap_type MetaData
 @wrap_type Engine
 @wrap_type Insert
 @wrap_type Connection
 @wrap_type Select
 @wrap_type ResultProxy
+@wrap_type BinaryExpression
+
+macro wrap_sql_type(typenames...)
+    e = Expr(:block)
+    for typename in typenames
+        sqlname = Symbol(string("SQL",typename))
+        q = quote
+            type $(esc(sqlname)) <: Wrapped
+                o::PyObject
+                function $(esc(sqlname))(args...; kwargs...)
+                    args = unwrap(args)
+                    kwargs = unwrap_kw(kwargs)
+                    new(sqlalchemy.$typename(args...; kwargs...))
+                end
+                $(esc(sqlname))(o::PyObject) = new(o)
+            end
+        end
+        push!(e.args, q)
+    end
+    e
+end
+
+@wrap_sql_type String Integer Boolean Date DateTime Enum Float Interval Numeric Text Time Unicode UnicodeText
+
+
+for (jl_type, sql_type) in [(Integer, SQLInteger), (Bool, SQLBoolean), (Real, SQLFloat), (String, SQLString)]
+    unwrap{T<:jl_type}(::Type{T}) = unwrap(sql_type())
+end
 
 type Other <: Wrapped
     o::PyObject
@@ -49,10 +78,13 @@ macro define_method(typename, method, ret)
     e = quote
         function $method(arg::$typename, args...; kwargs...)
             args = unwrap(args)
-            @show args
             kwargs = unwrap_kw(kwargs)
             val = unwrap(arg)[$(QuoteNode(method))](args...; kwargs...)
             $ret(val)
+        end
+
+        function $method(args...; kwargs...)
+            arg->$method(arg, args...; kwargs...)
         end
     end
     esc(e)
@@ -86,15 +118,25 @@ end
 @define_method Table alias Other
 @define_method Table join Other
 
-
 function Base.print(io::IO, w::Wrapped)
     print(io, unwrap(w)[:__str__]())
+end
+
+function Base.show(io::IO, w::Wrapped)
+    print(io, unwrap(w)[:__repr__]())
 end
 
 @define_top create_engine Engine
 @define_top select Select
 @define_top text Select
 
+getindex(t::Table, column_name) = Column(unwrap(t)[:c][Symbol(column_name)])
+
+for (op, py_op) in zip([:(==), :(>), :(>=), :(<), :(<=), :(!=)], [:__eq__, :__gt__, :__ge__, :__lt__, :__le__, :__ne__])
+    @eval function $op(c1::Column, c2::Union{Column, AbstractString, Number})
+        BinaryExpression(unwrap(c1)[$(QuoteNode(py_op))](unwrap(c2)))
+    end
+end
 
 
 end
