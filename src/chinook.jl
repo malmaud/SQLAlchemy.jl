6be92c5a04be221fdcc765085b9f7a74c98c4d55
Jon Malmaud <malmaud@gmail.com>
2015-07-08 17:15:37 -0400

    .

--- src/chinook.jl
+++ src/chinook.jl
@@ -1,16 +1,13 @@
 using Compat
 
-function loadchinook()
-    path = joinpath(Pkg.dir("SQLAlchemy"), "deps", "data", "Chinook_Sqlite.sqlite")
-    tables = ["Artist", "Album", "Playlist", "PlaylistTrack", "Track",
+const CHINOOK_TABLES = ["Artist", "Album", "Playlist", "PlaylistTrack", "Track",
               "MediaType", "Genre", "InvoiceLine", "Invoice", "Employee",
               "Customer"]
+
+function loadchinook()
+    path = joinpath(Pkg.dir("SQLAlchemy"), "deps", "data", "Chinook_Sqlite.sqlite")
     engine = createengine("sqlite:///$path")
-    meta = MetaData(engine)
-    schema = Dict{UTF8String, Table}()
-    for table in tables
-        schema[table] = Table(table, meta, autoload=true)
-    end
+    schema = MetaData(engine)
     db = connect(engine)
     db, schema
 end
